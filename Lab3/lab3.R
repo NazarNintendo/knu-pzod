bf4b4dd896021db91081091d367515819df0a092
nazar.nichen <nazar.nichen@gmail.com>
2022-11-09 20:34:54 +0200

    Lab 3

--- Lab3/lab3.R
+++ Lab3/lab3.R
@@ -1,7 +1,7 @@
 setwd("C:/Users/User/Desktop/FIT/PZOD/Lab3")
 data <- readRDS("hike_data.rds")
 
-data
+data[1, "features"]
 
 get_type <- function (x) strsplit(x, split = ", ")[[1]][2]
 
@@ -11,7 +11,7 @@ get_length <- function (x) {
   type <- strsplit(x, split = " ")[[1]][3]
   if (type == "one-way") {
     miles <- miles * 2
-  }
+  } 
   return(miles)
 }
 
@@ -27,4 +27,24 @@ clean_hike_trails <- data.frame(
   id = seq.int(nrow(data))
 )
 
-clean_hike_trails
+head(clean_hike_trails)
+
+# Question 1. How many routes have rating more than 4.9
+new_data <- subset(clean_hike_trails, rating > 4.9)
+nrow(new_data)
+
+# Question 2. How many routes are “Good for kids” (hint: you can use (unnest function)?
+new_data <- subset(unnest(data, cols = c("features")), features == "Good for kids")
+nrow(new_data)
+
+# Question 3. Which unique features can routes have?
+features <- unique(unnest(data, cols = c("features"))$features)
+features
+
+# Question 4. What is the most common rating of a route?
+tail(names(sort(table(clean_hike_trails$rating))), 1)
+
+# Question 5. Your own question and answer.
+# How many routes have highpoint more than 10000?
+new_data <- subset(clean_hike_trails, highpoint > 10000)
+nrow(new_data)
